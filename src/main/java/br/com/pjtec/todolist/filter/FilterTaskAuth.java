e9e5c4a905ebaca26252cfa6ebfc1aa8fc66ce1c
Paulo Costa <pjrc83@gmail.com>
2023-10-16 17:09:01 -0300

    Update tarefa

--- src/main/java/br/com/pjtec/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/pjtec/todolist/filter/FilterTaskAuth.java
@@ -25,7 +25,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 			throws ServletException, IOException {
 
 		var servletPath = request.getServletPath();
-		if (servletPath.equals("/tasks/")) {
+		if (servletPath.startsWith("/tasks/")) {
 
 			// O que p Once per Request Filter faz.
 
--- src/main/java/br/com/pjtec/todolist/task/TaskController.java
+++ src/main/java/br/com/pjtec/todolist/task/TaskController.java
@@ -1,12 +1,16 @@
 package br.com.pjtec.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,8 +45,17 @@ public class TaskController {
 		var task = this.taskRepository.save(taskModel);
 		return ResponseEntity.status(HttpStatus.OK).body(task);
 	}
-	public void list(HttpServletRequest request) {
+	@GetMapping("/")
+	public List<TaskModel> list(HttpServletRequest request) {
 		var idUser = request.getAttribute("idUser");
 		var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+		return tasks;
+	}
+	@PutMapping("/{id}")
+	public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+		var idUser = request.getAttribute("idUser");
+		taskModel.setIdUser((UUID) idUser);
+		taskModel.setId(id);
+	 return	this.taskRepository.save(taskModel);
 	}
 }
