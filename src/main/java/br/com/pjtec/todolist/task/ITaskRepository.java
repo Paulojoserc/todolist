1c41d72777a0d20b7e867db2a1b245b58146629c
Paulojoserc <pjrc83@gmail.com>
2023-10-16 23:02:53 -0300

    Validando Usuário Dono

--- src/main/java/br/com/pjtec/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/pjtec/todolist/task/ITaskRepository.java
@@ -8,5 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
 
 	List<TaskModel> findByIdUser(UUID idUser);
+	TaskModel findByIdAndIdUserList(UUID id, UUID idUser);
 
 }
